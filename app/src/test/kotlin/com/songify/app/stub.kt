7ef516589c2e1c7f927c9e8e55cc0cf47c9931b3
JohnBuhanan <johnbuns@yahoo.com>
2024-06-22 08:10:29 -0700

    refactor

--- app/src/test/kotlin/com/songify/app/stub.kt
+++
@@ -1,9 +0,0 @@
-package com.songify.app
-
-import java.lang.reflect.Proxy
-
-inline fun <reified T : Any> stub(): T =
-    Proxy.newProxyInstance(
-        T::class.java.classLoader,
-        arrayOf<Class<*>>(T::class.java)
-    ) { _, _, _ -> throw NotImplementedError("Stub!") } as T
