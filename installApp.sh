db163a0c5dbac975fe1dbec33720a2b4ee08292e
JohnBuhanan <johnbuns@yahoo.com>
2024-07-04 18:23:16 -0700

    refactor

--- installApp.sh
+++ installApp.sh
@@ -10,7 +10,4 @@ rm app-debug.apks
 bundletool build-apks --local-testing --bundle=app-debug.aab --output=app-debug.apks --ks=../../../../../app/debug.keystore --ks-key-alias=androiddebugkey --ks-pass=pass:android --key-pass=pass:android
 bundletool install-apks --apks app-debug.apks
 
-#jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore app/debug.keystore app-debug.aab keystoreAlias
-#jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA-1 -keystore  app-debug.aab androiddebugkey -keypass android -storepass android
-
 popd
