efb132d0833a4f77f8e2b17c9e9cc7ce1483e01a
JohnBuhanan <johnbuns@yahoo.com>
2024-07-08 13:57:17 -0700

    readme

--- build-logic/convention/src/main/kotlin/com/songify/extension/JetpackCompose.kt
+++ build-logic/convention/src/main/kotlin/com/songify/extension/JetpackCompose.kt
@@ -1,11 +1,8 @@
 package com.songify.extension
 
-import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.findByType
 
 fun Project.jetpackCompose() {
     applyOnce("org.jetbrains.kotlin.plugin.compose")
@@ -15,10 +12,8 @@ fun Project.jetpackCompose() {
     }
 
     val compilerVersion = libs.versions.composeCompiler.get()
-    extensions.findByType(LibraryExtension::class)?.apply {
-        jetpackComposeCompiler(compilerVersion)
-    }
-    extensions.findByType(ApplicationExtension::class)?.apply {
+
+    android {
         jetpackComposeCompiler(compilerVersion)
     }
 }
