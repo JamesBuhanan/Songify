24ab133bf76a138cd5289dd8c9d5fb87ace14fdc
tinder-johnbuhanan2 <john.buhanan@gotinder.com>
2025-05-13 12:49:16 -0700

    delete :feature:detail:app

---
+++ library/detail/fake/src/main/kotlin/com/songify/library/detail/fake/TestData.kt
@@ -0,0 +1,48 @@
+package com.songify.library.detail.fake
+
+import com.songify.library.spotify.model.SpotifyModel.Album
+import com.songify.library.spotify.model.SpotifyModel.Playlist
+import com.songify.library.spotify.model.SpotifyModel.Track
+
+object TestData {
+    val album = Album(
+        id = "1Mo4aZ8pdj6L1jx8zSwJnt",
+        caption = "THE TORTURED POETS DEPARTMENT",
+        imageUrlString = "https://i.scdn.co/image/ab67616d00001e025076e4160d018e378f488c33",
+        artistsString = "THE TORTURED POETS DEPARTMENT",
+        yearOfRelease = "2024",
+    )
+
+    val playlist = Playlist(
+        id = "37i9dQZF1DXcBWIGoYBM5M",
+        caption = "Today’s Top Hits",
+        imageUrlString = "https://i.scdn.co/image/ab67706f000000028695a5a2512e4c614fdbfc39",
+        name = "Today’s Top Hits",
+        ownerName = "Spotify",
+        totalNumberOfTracks = "50",
+    )
+
+    val tracks = listOf(
+        Track(
+            id = "id",
+            caption = "caption",
+            imageUrlString = "imageUrlString",
+            artistsString = "artistsString",
+            trackUrlString = null,
+        ),
+        Track(
+            id = "id",
+            caption = "caption",
+            imageUrlString = "imageUrlString",
+            artistsString = "artistsString",
+            trackUrlString = null,
+        ),
+        Track(
+            id = "id",
+            caption = "caption",
+            imageUrlString = "imageUrlString",
+            artistsString = "artistsString",
+            trackUrlString = null,
+        )
+    )
+}
--- library/detail/fake/src/main/kotlin/com/songify/library/detail/fake/usecase/FakeGetTracks.kt
+++ library/detail/fake/src/main/kotlin/com/songify/library/detail/fake/usecase/FakeGetTracks.kt
@@ -1,13 +1,15 @@
 package com.songify.library.detail.fake.usecase
 
 import androidx.paging.PagingData
+import com.songify.library.detail.fake.TestData
 import com.songify.library.detail.usecase.GetTracks
 import com.songify.library.spotify.model.SpotifyModel
 import com.songify.library.spotify.model.SpotifyModel.Track
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 object FakeGetTracks : GetTracks {
     override suspend fun invoke(spotifyModel: SpotifyModel): Flow<PagingData<Track>> {
-        TODO("Not yet implemented")
+        return flowOf(PagingData.from(TestData.tracks))
     }
 }
